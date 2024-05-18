bda6358ccc65c9b206e0c9d91b86f5a0931164a6
Qusay3 <Ahmqusay7@gmail.com>
2024-05-18 04:49:27 -0400

    added new files

---
+++ arrays.py
@@ -0,0 +1,22 @@
+new_list = [1,2,3]
+result = new_list[0]
+print(result)
+
+if 1 in new_list: print (True)
+
+for n in new_list:
+    if n == 1:
+        print(True)
+
+        break
+
+new_list.append(5)
+list
+len(list)
+
+new_list.insert(4, 6)
+print(list[4])
+
+new_list.extend([8,9,10])
+
+
